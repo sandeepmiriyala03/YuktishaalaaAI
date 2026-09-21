075f8755e96e1bd2aac9c4a700d087254f1460dc
Sandeep Miriyala <vanisandeep@gmail.com>
2026-09-21 18:33:22 +0530

    ok code

---
+++ excerise2.py
@@ -0,0 +1,23 @@
+import math
+import random 
+
+#angle = int(input("Enter angle in degrees: "))
+#radians = math.radians(angle)
+
+#print(f"The sine of {angle} degrees is: {math.sin(radians)}")
+#print(f"The cosine of {angle} degrees is: {math.cos(radians):.3f}")
+
+#print(f"The tangent of {angle} degrees is: around {math.tan(radians):.2f}")
+
+random.seed(5)
+print("Random number between 0 and 1:", random.randint(1, 5))
+
+
+##problem 2.c 
+
+random.seed(5)
+print("Random number between 0 and 1:", random.randint(10, 50))
+random.seed(6)
+print("Random number between 0 and 1:", random.randint(10, 50))
+
+
